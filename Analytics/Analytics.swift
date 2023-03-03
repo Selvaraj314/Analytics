3c07e5d057012d7fbd053168096bcf64fffdaf71
Selvaraj <selvaraj314@gmail.com>
2023-03-03 22:30:43 +0800

    firstCommit

---
+++ Analytics/Analytics.swift
@@ -0,0 +1,17 @@
+//
+//  Analytics.swift
+//  AnalyticsFramework
+//
+//  Created by Thangaselvam saraswathy on 03/03/23.
+//
+
+import Foundation
+
+
+public class Analytics {
+    public init() {}
+    
+    public func log(message: String) {
+        print("Log message: ", message)
+    }
+}
---
+++ AnalyticsFramework/AnalyticsFramework.h
@@ -0,0 +1,18 @@
+//
+//  AnalyticsFramework.h
+//  AnalyticsFramework
+//
+//  Created by Thangaselvam saraswathy on 03/03/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AnalyticsFramework.
+FOUNDATION_EXPORT double AnalyticsFrameworkVersionNumber;
+
+//! Project version string for AnalyticsFramework.
+FOUNDATION_EXPORT const unsigned char AnalyticsFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AnalyticsFramework/PublicHeader.h>
+
+
